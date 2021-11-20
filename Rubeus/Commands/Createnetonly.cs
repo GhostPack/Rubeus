52d5ac40d4c64cc1c0faf1b748ea4eae414eae14
Mike Loss <mike.loss@cybercx.com.au>
2021-11-20 20:36:15 +0800

    tidied up all that conditional mess, added some helpful error messages.

--- Rubeus/Commands/Createnetonly.cs
+++ Rubeus/Commands/Createnetonly.cs
@@ -12,36 +12,50 @@ namespace Rubeus.Commands
         {
             Console.WriteLine("\r\n[*] Action: Create Process (/netonly)\r\n");
 
+            string program = null;
+            string username = null;
+            string password = null;
+            string domain = null;
+            bool show = arguments.ContainsKey["/show"];
+
             if (arguments.ContainsKey("/program"))
             {
-                if (arguments.ContainsKey("/show"))
-                {
-                    if (arguments.ContainsKey("/username"))
-                    {
-                        Helpers.CreateProcessNetOnly(arguments["/program"], true, arguments["/username"], arguments["/domain"], arguments["/password"]);
-                    }
-                    else
-                    {
-                        Helpers.CreateProcessNetOnly(arguments["/program"], true);
-                    }
-                }
-                else
-                {
-                    if (arguments.ContainsKey("/username"))
-                    {
-                        Helpers.CreateProcessNetOnly(arguments["/program"], true, arguments["/username"], arguments["/domain"], arguments["/password"]);
-                    }
-                    else
-                    {
-                        Helpers.CreateProcessNetOnly(arguments["/program"]);
-                    }
-                }
+                program = arguments["/program"];
             }
-
             else
             {
                 Console.WriteLine("\r\n[X] A /program needs to be supplied!\r\n");
+                return;
             }
+
+            if (arguments.ContainsKey("/username"))
+            {
+                username = arguments["/username"];
+            }
+            if (arguments.ContainsKey("/password"))
+            {
+                password = arguments["/password"];
+            }
+            if (arguments.ContainsKey("/domain"))
+            {
+                domain = arguments["/domain"];
+            }
+
+            if (username == null && password == null && domain == null)
+            {
+                Console.WriteLine("\r\n[*] Using random username and password.\r\n");
+                Helpers.CreateProcessNetOnly(program, show, username, domain, password);
+                return;
+            }
+
+            if (!String.IsNullOrWhiteSpace(username) && !String.IsNullOrWhiteSpace(password) && !String.IsNullOrWhiteSpace(domain))
+            {
+                Console.WriteLine("\r\n[*] Using " + domain + "\\" + username + ":" + password + ".\r\n");
+                Helpers.CreateProcessNetOnly(program, show, username, domain, password);
+                return;
+            }
+
+            Console.WriteLine("\r\n[X] Explicit creds require /username, /password, and /domain to be supplied!\r\n");
         }
     }
 }
