46b9d96b9cfc01732fce4bad7f9299f9d811e6b6
anthemtotheego <anthemtotheego@users.noreply.github.com>
2022-02-18 08:27:48 -0600

    Update Helpers.cs
    Replaced System.Environment.Exit(1); with return null;  This will prevent the process from being killed if ran inline (in a non sacrificial process).

--- Rubeus/lib/Helpers.cs
+++ Rubeus/lib/Helpers.cs
@@ -59,7 +59,7 @@ namespace Rubeus
             if ((hex.Length % 16) != 0)
             {
                 Console.WriteLine("\r\n[X] Hash must be 16, 32 or 64 characters in length\r\n");
-                System.Environment.Exit(1);
+                return null;
             }
 
             // yes I know this inefficient
